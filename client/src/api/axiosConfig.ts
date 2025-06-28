bc0567c0e891cde7d15726241d5adcdccc57b95e
yvishwas40 <yvishwas40@gmail.com>
2025-06-28 11:51:15 +0530

    added the backend server link direct to code

--- client/src/api/axiosConfig.ts
+++ client/src/api/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = "https://ai-journal-app-i3ss.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
