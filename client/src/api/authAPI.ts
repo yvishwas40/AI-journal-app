69f7bfdb00b44b0d5603b5235409955af48e1ca2
yvishwas40 <yvishwas40@gmail.com>
2025-06-28 10:57:39 +0530

    all the api end points are working

--- client/src/api/authAPI.ts
+++ client/src/api/authAPI.ts
@@ -10,3 +10,15 @@ export const authAPI = {
   verifyToken: () =>
     api.get('/auth/verify'),
 };
+// import api from './axiosConfig';
+
+// export const authAPI = {
+//   login: (email: string, password: string) =>
+//     api.post('/api/auth/login', { email, password }),
+
+//   register: (name: string, email: string, password: string) =>
+//     api.post('/api/auth/register', { name, email, password }),
+
+//   verifyToken: () =>
+//     api.get('/api/auth/verify'),
+// };
